61e30f64f3555afed0bfa9bbe4da63f3dbec3a51
icodeunot <jsal.dev@protonmail.com>
2025-04-07 22:25:43 -0700

    Adding RSS feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog'
@@ -21,4 +22,5 @@ urlpatterns = [
     path(
         '<int:post_id>/comment/', views.post_comment, name='post_comment'
     ),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
