a56ae5f962a00e2d8732ad1afb653d2f84bd9dfb
icodeunot <jsal.dev@protonmail.com>
2025-04-10 00:36:07 -0700

    Migrate DB, create search query options.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from .forms import CommentForm, EmailPostForm
+from django.contrib.postgres.search import SearchVector
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 from taggit.models import Tag
 
@@ -151,5 +152,31 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment
+        }
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                )
+                .filter(search=query)
+            )
+
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
